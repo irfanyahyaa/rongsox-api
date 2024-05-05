611276e6d789ffcd86e6b531fb1b2dafd5e8d86b
achmad musfiul chaggi <musfiulchaggi@gmail.com>
2024-05-05 09:58:48 +0700

    initialize commit

---
+++ src/test/java/com/enigma/bank_sampah/BankSampahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.bank_sampah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSampahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
