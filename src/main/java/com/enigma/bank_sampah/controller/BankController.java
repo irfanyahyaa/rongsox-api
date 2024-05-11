45f21b0284e2d8e8f39ae20116e4d2ebee905514
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-05-12 02:26:59 +0700

    [FIX] fix get all for bank api

--- src/main/java/com/enigma/bank_sampah/controller/BankController.java
+++ src/main/java/com/enigma/bank_sampah/controller/BankController.java
@@ -59,15 +59,19 @@ public class BankController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CommonResponse<List<BankResponse>>> getAllBank(
+    public ResponseEntity<CommonResponse<List<BankResponse>>> getAllBanks(
             @RequestParam(name = "page", defaultValue = "1") Integer page,
             @RequestParam(name = "size", defaultValue = "10") Integer size,
+            @RequestParam(name = "sortBy", defaultValue = "name") String sortBy,
+            @RequestParam(name = "direction", defaultValue = "asc") String direction,
             @RequestParam(name = "q", required = false) String query
 
     ){
         SearchBankRequest request = SearchBankRequest.builder()
                 .page(Math.max(page - 1, 0))
                 .size(size)
+                .sortBy(sortBy)
+                .direction(direction)
                 .query(query)
                 .build();
 
